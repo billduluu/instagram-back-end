ed103040397006ded31fd8b389bf0669f75d3ae8
B. Bilguundul <billduluu@gmail.com>
2021-10-01 11:20:32 +0800

    register api

--- models/users.js
+++ models/users.js
@@ -1,16 +1,29 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const { isEmail } = validator;
+const bcrypt = require("bcrypt");
 const { Schema } = mongoose;
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
+  fullname: { type: String },
+  password: { type: String, required: true },
+  hash: { type: String },
   email: {
     type: String,
     required: true,
     lowercase: true,
+    unique: true,
     validate: [isEmail, "invalid email"],
   },
   createdAt: { type: Date, default: Date.now },
   lastLogin: { type: Date },
+});
+
+userSchema.pre("save", async function (next) {
+  const rounds = 10; // What you want number for round paasword
+
+  const hash = await bcrypt.hash(this.password, rounds);
+  this.password = hash;
+  next();
 });
 module.exports = mongoose.model("users", userSchema);
--- routes/useUsers.js
+++ routes/useUsers.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const userModel = require("../models/users");
 const app = express();
-app.get("/users", async (request, response) => {
-  const users = await userModel.find({});
+app.get("/login", async (request, response) => {
+  const users = await userModel.findOne(request.body);
   try {
     response.send(users);
   } catch (error) {
     response.status(500).send(error);
   }
 });
-app.post("/user", async (request, response) => {
-  const user = new userModel(request.body);
+app.post("/register", async (request, response) => {
+  const { fullname, username, email, password } = request.body;
+  let user = new userModel({ fullname, username, email, password });
   try {
+    if (!(email && password && fullname && username)) {
+      response.status(400).send("All input is required");
+    }
+    const oldUser = await userModel.findOne({ email });
+
+    if (oldUser) {
+      return response.status(409).send("User Already Exist. Please Login");
+    }
     await user.save();
-    response.send(user);
+    response.status(201);
   } catch (error) {
     response.status(500).send(error);
   }
